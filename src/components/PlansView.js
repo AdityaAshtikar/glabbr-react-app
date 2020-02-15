55fabb5401290f316cfca38553d8ec6138b92a56
AdityaAshtikar <ashtikar.aditya97@gmail.com>
2020-02-15 17:49:15 +0530

    added loader

--- src/components/PlansView.js
+++ src/components/PlansView.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import PlanSummary from './PlanSummary'
 import axios from "axios";
 import PlanToggler from './PlanToggler';
+import Loader from './Loader';
 
 export class PlansView extends Component {
 
     state = {
         isMonthly: true,
+        loading: false,
         plans: []
     }
 
@@ -19,9 +21,13 @@ export class PlansView extends Component {
     async componentDidMount () {
                 // https://www.mocky.io/v2/5e3156863200007200888450
         let url = 'https://www.mocky.io/v2/5e3156863200007200888450'
+        this.setState({loading: true})
         await axios.get(url)
-                .then(res => this.setState({plans: res.data.plans}))
-                .catch(error => console.log("Error: ", error))
+                .then(res => this.setState({plans: res.data.plans, loading: false}))
+                .catch(error => {
+                    console.log("Error: ", error); 
+                    return this.setState({ loading: false });
+                })
     }
 
     render() {
@@ -31,7 +37,7 @@ export class PlansView extends Component {
         return (
             <div className="container vertically-centered">
                 <PlanToggler changeState={this.changeState} />
-                <PlanSummary plans={plans} />
+                { this.state.loading ? <Loader /> : <PlanSummary plans={plans} /> }
             </div>
         )
     }
