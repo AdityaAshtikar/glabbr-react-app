beca181e640f8923f59f268ed59fac6be582c69d
AdityaAshtikar <ashtikar.aditya97@gmail.com>
2020-02-15 17:23:26 +0530

    Updates

---
+++ precache-manifest.794acc23a3115a16ccb1b7fd94547082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def3b01aad564c9e876cd1b53c062610",
+    "url": "/glabbr-react-app/index.html"
+  },
+  {
+    "revision": "393043b57cebb7ccdeb5",
+    "url": "/glabbr-react-app/static/css/main.a246d239.chunk.css"
+  },
+  {
+    "revision": "b8b0d8384d909214320f",
+    "url": "/glabbr-react-app/static/js/2.fea8cba0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/glabbr-react-app/static/js/2.fea8cba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393043b57cebb7ccdeb5",
+    "url": "/glabbr-react-app/static/js/main.df05e66c.chunk.js"
+  },
+  {
+    "revision": "f53814adef40807a8060",
+    "url": "/glabbr-react-app/static/js/runtime-main.7e0b0e9a.js"
+  }
+]);
